813addec6f9649aa14c31a8932d0f43e059d9788
Cameron Brown <cameron.brown@futurenet.com>
2024-02-13 23:04:55 +0000

    Remove jest comments

--- jest.config.js
+++ jest.config.js
@@ -7,35 +7,3 @@ const config = {
 };
 
 module.exports = config;
-// module.exports = {
-//   modulePaths: ['./src'],
-//   moduleFileExtensions: ['ts', 'tsx', 'jsx', 'js', 'json'],
-//   transform: {
-//     '^.+\\.(t|j)sx?$': [
-//       '@swc/jest',
-//       {
-//         jsc: {
-//           transform: {
-//             hidden: {
-//               jest: true,
-//             },
-//           },
-//         },
-//       },
-//     ],
-//   },
-//   testMatch: ['<rootDir>/src/**/*/?(*.)+(spec|test).[jt]s?(x)'],
-//   moduleNameMapper: {
-//     '^log(.*)$': '<rootDir>/src/log/$1',
-//     '^metrics(.*)$': '<rootDir>/src/metrics/$1',
-//     '^third-party-apis(.*)$': '<rootDir>/src/third-party-apis/$1',
-//     '^iframe-api(.*)$': '<rootDir>/src/ad-tool/load/api/$1',
-//     '^devias(.*)$': '<rootDir>/src/ad-tool/src/devias/$1',
-//     '^ad-tool(.*)$': '<rootDir>/src/ad-tool/src/$1',
-//     '^utils(.*)$': '<rootDir>/src/utils/$1',
-//   },
-//   transformIgnorePatterns: ['node_modules/(?!(@future/promjs)/)'],
-//   testEnvironment: 'jsdom',
-//   testEnvironmentOptions: { resources: 'usable' },
-//   collectCoverageFrom: ['src/**/*.{ts,js,json}'],
-// };
