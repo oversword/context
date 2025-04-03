99b14ed7ac67b0cbb28d08180d1b1e06c25ae0f4
Cameron Brown <cameron.brown@futurenet.com>
2025-04-03 17:42:18 +0100

    Better menu inheritance

--- src/system/default-config.ts
+++ src/system/default-config.ts
@@ -1,25 +1,34 @@
 import { MENU_ITEM_ID, MENU_ITEM_PARENT } from '@/constants/menu-item'
-import { ContextMenuItemMode } from '@/types/index.types'
+import { ContextMenuItemList, ContextMenuItemMode, ContextParentMenuMeta } from '@/types/index.types'
 import { ContextSystemConfig } from '@/types/system.types'
 
+const getParentMenu = (parentInfo: ContextParentMenuMeta): ContextMenuItemList => {
+	if (parentInfo && parentInfo.menu.length) {
+		if (parentInfo.menu.length === 1 && parentInfo.menu[0][MENU_ITEM_PARENT]) {
+			return parentInfo.menu
+		} else {
+			return [{
+				mode: ContextMenuItemMode.section,
+				label: parentInfo.label,
+				children: parentInfo.menu,
+				[MENU_ITEM_ID]: parentInfo[MENU_ITEM_ID],
+				[MENU_ITEM_PARENT]: true
+			}]
+		}
+	}
+	return []
+}
 const defaultConfiguration: ContextSystemConfig = {
 	strategy_mergeMenu: (staticMenu) => (_action, current, parentInfo) => {
 		const cleanCurrent = current.filter(item => !(
 			item[MENU_ITEM_PARENT] &&
 			item[MENU_ITEM_ID] === parentInfo[MENU_ITEM_ID]
 		))
+
 		return [
 			...staticMenu,
 			...cleanCurrent,
-			...(parentInfo && parentInfo.menu.length
-				? [{
-					mode: ContextMenuItemMode.section,
-					label: parentInfo.label,
-					children: parentInfo.menu,
-					[MENU_ITEM_ID]: parentInfo[MENU_ITEM_ID],
-					[MENU_ITEM_PARENT]: true
-				}]
-				: [])
+			...getParentMenu(parentInfo),
 		]
 	},
 	strategy_mergeActs: (staticActs) => (_action, current) => Object.entries(staticActs).reduce((current, [actionName, options]) => ({
